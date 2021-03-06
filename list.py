30092481c2184bc5f9ffa3330103539c770a9ae4
leahcimto <68165163+leahcimto@users.noreply.github.com>
2021-03-06 16:06:36 -0500

    e

---
+++ list.py
@@ -0,0 +1,10 @@
+forumhelpers = ['paddle', 'zach', 'ihatr', 'bluebatstar', '-accio-', 'ssb20', 'meeeeeeeeee']
+
+print(forumhelpers[0].title())
+
+
+
+
+print(forumhelpers[1].title())
+
+print(forumhelpers[2].title())
